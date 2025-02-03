14f9e3db686e698e3b16e15c04815c9217d0018c
jackrichter <jackrichter1@gmail.com>
2025-02-03 14:57:42 +0100

    Validation implemented for the values in the LoansDto class.

--- src/main/java/com/eazybytes/loans/dto/LoansDto.java
+++ src/main/java/com/eazybytes/loans/dto/LoansDto.java
@@ -1,19 +1,31 @@
 package com.eazybytes.loans.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Data;
 
 @Data
 public class LoansDto {
 
+    @NotEmpty(message = "Mobile Number can not be a null or empty")
+    @Pattern(regexp = "", message = "")
     private String mobileNumber;
 
+    @NotEmpty(message = "Loan Number can not be a null or empty")
+    @Pattern(regexp = "", message = "")
     private String loanNumber;
 
+    @NotEmpty(message = "LoanType can not be a null or empty")
     private String loanType;
 
+    @Positive(message = "Total loan amount should be greater than zero")
     private int totalLoan;
 
+    @PositiveOrZero(message = "Total loan amount paid should be equal or greater than zero")
     private int amountPaid;
 
+    @PositiveOrZero(message = "Total outstanding amount should be equal or greater than zero")
     private int outstandingAmount;
 }
